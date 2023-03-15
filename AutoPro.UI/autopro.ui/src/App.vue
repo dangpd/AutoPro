d4dad18df6059fb9408af6bec46302c333ac6c24
Dangpd <phamducdang0403@gmail.com>
2023-03-15 18:57:17 +0700

    new commit

---
+++ AutoPro.UI/autopro.ui/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <IndexWeb></IndexWeb>
+  </div>
+</template>
+
+<script>
+
+import IndexWeb from './layout/IndexWeb.vue';
+export default {
+  name: 'App',
+  components: {
+    IndexWeb,
+  }
+}
+</script>
+
+<style>
+</style>
